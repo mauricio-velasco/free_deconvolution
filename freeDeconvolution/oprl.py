f4510076ed00541d0114633a791a721ee80473f9
redachhaibi <chhaibi.reda@gmail.com>
2022-08-14 09:05:21 +0200

    :tada: Working OPRL reconstruction

--- freeDeconvolution/oprl.py
+++ freeDeconvolution/oprl.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 # Get indices of diagonal and super-diagonal for dimension dim
 def jacobi_indices( dim ):
@@ -7,22 +8,33 @@ def jacobi_indices( dim ):
     extra_diag_indices[1,:] += 1
     return diagonal_indices, extra_diag_indices
 
-# Input: Array of 2n+1 moments from c_0 to c_{2n}
-from scipy.linalg import cholesky as scipy_cholesky
+# Input: Array of 2n+1 moments from c_0=1 to c_{2n+1}
 def jacobi_from_moments( mom_array, debug=False ):
-    n = int( 0.5*(len(mom_array)-1) )
-    mom_matrix = np.zeros( shape=(n+1, n+1) )
-    for index in range( n+1 ):
-        mom_matrix[index,:] = mom_array[index:(index+n+1)]
+    assert( (len(mom_array)-1) % 2 == 1 )
+    mom_array = np.hstack( (mom_array, [0]))
+    #
+    n = int( 0.5*(len(mom_array)-2) )
+    mom_matrix = np.zeros( shape=(n+2, n+2) )
+    for index in range( n+2 ):
+        mom_matrix[index,:] = mom_array[index:(index+n+2)]
 
     # Cholesky
-    try:
-        cholesky = scipy_cholesky( mom_matrix, lower=True )
-        print("Cholesky passed!")
-        print("")
-    except np.linalg.LinAlgError as err:
-        print( err  )
-        print("")
+    # try:
+    #     cholesky = scipy_cholesky( mom_matrix, lower=True )
+    #     print("Cholesky passed!")
+    #     print("")
+    # except np.linalg.LinAlgError as err:
+    #     print( err  )
+    #     print("")
+
+    # Use of LAPACK wrapper in scipy
+    # https://stackoverflow.com/questions/49101574/scipy-numpy-cholesky-while-checking-if-positive-definite
+    (cholesky, minor) = scipy.linalg.lapack.dpotrf( mom_matrix, lower=True  )
+    if minor > 0:
+        print( f'''{minor}-th principal minor is not positive definite''')
+        mom_matrix = mom_matrix[:minor, :minor]
+        #
+        n = minor - 1
 
     if debug:
         print( "Cholesky matrix: ")
@@ -30,25 +42,24 @@ def jacobi_from_moments( mom_array, debug=False ):
 
     mom_count = cholesky.shape[0]
     diag_indices, extra_indices = jacobi_indices( mom_count )
-    #print( diag_indices )
-    #print( extra_indices )
     diag_cholesky = cholesky.T[diag_indices[0,:] , diag_indices[1,:]]
     extra_diag    = cholesky.T[extra_indices[0,:], extra_indices[1,:]]
-    print( "Diagonal of Cholesky")
-    print( diag_cholesky )
-    print( "Extra-diagonal of Cholesky")
-    print( extra_diag )
-    print("")
+
+    if debug:
+        print( "Diagonal of Cholesky")
+        print( diag_cholesky )
+        print( "Extra-diagonal of Cholesky")
+        print( extra_diag )
+        print("")
 
     # Compute Jacobi
     jacobi_b = diag_cholesky[1:]/diag_cholesky[:-1]
-    jacobi_a = np.zeros_like( diag_cholesky )[:-1]
-    jacobi_a[0] = extra_diag[0]
-    #x_over_y = extra_diag[1:]/diag_cholesky[1:-1]
-    #jacobi_a[2:-1] = x_over_y[1:]-x_over_y[:-1]
-    x_over_y = extra_diag/diag_cholesky[:-1]
-    jacobi_a[1:] = x_over_y[1:]-x_over_y[:-1]
     jacobi_b = jacobi_b[:-1]
+    jacobi_a = np.zeros_like( extra_diag )
+    if len(extra_diag)>0:
+        jacobi_a[0] = extra_diag[0]
+        x_over_y = extra_diag/diag_cholesky[:-1]
+        jacobi_a[1:] = x_over_y[1:]-x_over_y[:-1]
 
     return jacobi_a, jacobi_b
 
@@ -65,7 +76,13 @@ def quadrature_from_jacobi( jacobi_a, jacobi_b, debug=False):
         print( jacobi )
         print( "" )
 
+    # Diagonalize
     eigen, vectors = np.linalg.eig(jacobi)
     cyclic_vector = vectors[0,:]
+
+    # Sort
+    permutation = np.argsort( eigen )
+    eigen = eigen[ permutation ]
+    cyclic_vector = cyclic_vector[ permutation ]
 
     return eigen, cyclic_vector*cyclic_vector
